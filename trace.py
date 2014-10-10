6e999ff309d33a5396335effac74845072cb9052
Niklas Q. Nielsen <niklas@mesosphere.io>
2014-10-09 22:46:48 -0700

    Wired up histogram for task duration

--- trace.py
+++ trace.py
@@ -1,14 +1,16 @@
 import sys
 import re
 import numpy
+import matplotlib.pyplot as plt
 
 from datetime import datetime, date, time
 from itertools import ifilter
 
 # TODO(nnielsen): Wire up task per framework to avoid collision.
 class framework:
-    def __init__(self):
-        pass
+    def __init__(self, framework_id):
+        self.framework_id = framework_id
+        self.tasks = {}
 
 class task:
     def __init__(self, task_id, cpus, mem):
@@ -39,7 +41,7 @@ def stat(title, data_points):
     maximum = max(data_points)
     minimum = min(data_points)
 
-    print "%s [ mean: %f\t stdev: %f\tmax: %f\tmin: %f ]" % (title, mean, stdev, maximum, minimum)
+    print "%s [ mean: %f\t stdev: %f\tmax: %f\tmin: %f samples: %d ]" % (title, mean, stdev, maximum, minimum, len(data_points))
 
 re_is_task_status = re.compile('.*] Status update.*')
 re_is_launch = re.compile('.*] Launching task.*')
@@ -58,10 +60,10 @@ def main():
 
     filtered_list = ifilter(is_task_status_or_launch, open(sys.argv[1]))
 
-    tasks = {}
+    frameworks = {}
 
-    filter_task = re.compile("I\d+ (.*)  \d+ master\.cpp.* Status update (\w+) .* for task (.*) of framework (.*)")
-    filter_launch = re.compile("I\d+ (.*)  \d+ master\.cpp.* Launching task (.*) of framework (.*) with resources (.*) on slave.*")
+    filter_task = re.compile("I\d+ (.*) \d+ master\.cpp.* Status update (\w+) .* for task (.*) of framework (.*) from slave.*")
+    filter_launch = re.compile("I\d+ (.*) \d+ master\.cpp.* Launching task (.*) of framework ([0-9\-]+) .* with resources (.*) on slave.*")
     filter_resources = re.compile("cpus\(\*\):([0-9\.]+); mem\(\*\):(\d+).*")
     for line in filtered_list:
         task_m = filter_task.match(line)
@@ -69,14 +71,19 @@ def main():
             timestamp = datetime.strptime(task_m.group(1), "%H:%M:%S.%f")
             status = task_m.group(2)
             task_id = task_m.group(3)
+            framework_id = task_m.group(4)
 
-            if task_id not in tasks:
+            if framework_id not in frameworks:
+                continue
+
+            f = frameworks[framework_id]
+            if task_id not in f.tasks:
                 continue
 
             if (status == "TASK_RUNNING"):
-                tasks[task_id].started(status, timestamp)
+                f.tasks[task_id].started(status, timestamp)
             elif (status == "TASK_FINISHED") or (status == "TASK_FAILED") or (status == "TASK_LOST"):
-                tasks[task_id].terminated(status, timestamp)
+                f.tasks[task_id].terminated(status, timestamp)
         else:
             launch_m = filter_launch.match(line)
             if launch_m is not None:
@@ -92,22 +99,37 @@ def main():
                     cpus = float(resources_m.group(1))
                     mem = int(resources_m.group(2))
 
-                tasks[task_id] = task(task_id, cpus, mem)
+                if framework_id not in frameworks:
+                    frameworks[framework_id] = framework(framework_id)
+                f = frameworks[framework_id]
+
+                f.tasks[task_id] = task(task_id, cpus, mem)
 
     durations = []
     cpus_list = []
     mem_list = []
-    for task_id in tasks:
-        t = tasks[task_id]
-        d = t.duration()
-        if d is not None:
-          durations.append(d.total_seconds())
-        cpus_list.append(t.cpus)
-        mem_list.append(t.mem)
+    for framework_id in frameworks:
+        f = frameworks[framework_id]
+        for task_id in f.tasks:
+            t = f.tasks[task_id]
+            d = t.duration()
+            if d is not None:
+              durations.append(d.total_seconds())
+            cpus_list.append(t.cpus)
+            mem_list.append(t.mem)
 
     stat("duration:\t", durations)
     stat("cpus:\t\t", cpus_list)
     stat("mem:\t\t", mem_list)
+
+    num_bins = 50
+    n, bins, patches = plt.hist(durations, num_bins, normed=0, facecolor='green', alpha=0.5)
+    plt.xlabel('Duration (seconds)')
+    plt.ylabel('Frequency')
+    plt.title('Task duration')
+
+    plt.subplots_adjust(left=0.15)
+    plt.savefig('durations.png')
 
 
 if __name__ == "__main__":
